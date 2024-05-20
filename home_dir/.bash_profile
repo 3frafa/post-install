6f9c5db6d8ff37864eece02971d513669bd99e9a
3frafa <167480425+3frafa@users.noreply.github.com>
2024-05-20 22:33:36 +0200

    init

---
+++ home_dir/.bash_profile
@@ -0,0 +1,8 @@
+export EDITOR=nvim
+export VISUAL=nvim
+
+export MOZ_ENABLE_WAYLAND=1
+export PATH=$PATH:~/bin
+export XDG_CURRENT_DESKTOP=sway
+
+[[ -f ~/.bashrc ]] && . ~/.bashrc
---
+++ post-install.sh
@@ -0,0 +1,90 @@
+#!/usr/bin/env bash
+
+pkgs=(
+    arc-icon-theme
+    arc-solid-gtk-theme
+    bc
+    brightnessctl
+    btop
+    dbus-glib
+    dunst
+    engrampa
+    fd
+    ffmpeg
+    firefox
+    foot
+    fzf
+    gammastep
+    gnome-disk-utility
+    gnome-keyring
+    gparted
+    grim
+    gvfs
+    gvfs-mtp
+    i3status
+    keepassxc
+    kid3-qt
+    man-db
+    mpv
+    neovim
+    noto-fonts
+    noto-fonts-cjk
+    noto-fonts-emoji
+    noto-fonts-extra
+    nsxiv
+    ntfs-3g
+    openresolv
+    p7zip
+    pulsemixer
+    qbittorrent
+    ristretto
+    rsync
+    shellcheck
+    slurp
+    sway
+    swaybg
+    swaylock
+    thunar
+    thunar-archive-plugin
+    thunar-volman
+    wireguard-tools
+    wl-clipboard
+    xdg-desktop-portal-gtk
+    xdg-desktop-portal-wlr
+    xdg-user-dirs
+    xorg-xwayland
+    yt-dlp
+)
+
+pkgs_aur=(
+    mp3val
+    nwg-displays
+)
+
+# pkg installation
+    sudo sed -i 's/#Color/Color/' /etc/pacman.conf
+    sudo pacman --needed --noconfirm -Syu "${pkgs[@]}" || exit 1
+
+    command -v yay >/dev/null 2>&1 ||
+        git clone https://aur.archlinux.org/yay-git.git &&
+            (cd yay-git && makepkg --noconfirm -si)
+    yay --needed --noconfirm -Syu "${pkgs_aur[@]}" || exit 1
+
+# dotfiles
+    cp -r home_dir/. ~
+    chmod +x ~/bin/*
+    curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim  || exit 1
+
+# config (auto)
+    gsettings set org.gnome.desktop.interface gtk-theme  'Arc-Dark-solid'
+    gsettings set org.gnome.desktop.interface icon-theme 'Arc'
+
+    nvim +PlugInstall +q +q
+    sudo sed -i 's/#NoUpgrade   =/NoUpgrade   = \/usr\/share\/applications\/nvim.desktop/' /etc/pacman.conf
+    sudo sed -i 's/Exec=nvim %F/Exec=footclient nvim %F/' /usr/share/applications/nvim.desktop
+    sudo sed -i 's/Terminal=true/Terminal=false/'         /usr/share/applications/nvim.desktop
+
+# config (manual)
+    sudo nvim /etc/xdg/user-dirs.defaults
+    xdg-user-dirs-update
